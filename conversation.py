edfa4d0a1975d3398b6cf0c3b9f6f9e083f1a4cb
guozhaokang <guozhaokang@bytedance.com>
2024-10-10 15:17:36 +0800

    glm4_tool vllm

---
+++ conversation.py
@@ -0,0 +1,144 @@
+import json
+import re
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum, auto
+
+import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
+
+from PIL.Image import Image
+
+QUOTE_REGEX = re.compile(r"【(\d+)†(.+?)】")
+
+SELFCOG_PROMPT = "你是一个名为 GLM-4 的人工智能助手。你是基于智谱AI训练的语言模型 GLM-4 模型开发的，你的任务是针对用户的问题和要求提供适当的答复和支持。"
+DATE_PROMPT = "当前日期: %Y-%m-%d"
+
+
+def build_system_prompt(
+    functions: list[dict],
+):
+    value = SELFCOG_PROMPT
+    value += "\n\n" + datetime.now().strftime(DATE_PROMPT)
+    value += "\n\n# 可用工具"
+    contents = []
+    for function in functions:
+        content = f"\n\n## {function['name']}\n\n{json.dumps(function, ensure_ascii=False, indent=4)}"
+        content += "\n在调用上述函数时，请使用 Json 格式表示调用的参数。"
+        contents.append(content)
+    value += "".join(contents)
+    return value
+
+
+def response_to_str(response: str | dict[str, str]) -> str:
+    """
+    Convert response to string.
+    """
+    if isinstance(response, dict):
+        return response.get("name", "") + response.get("content", "")
+    return response
+
+
+class Role(Enum):
+    SYSTEM = auto()
+    USER = auto()
+    ASSISTANT = auto()
+    TOOL = auto()
+    OBSERVATION = auto()
+
+    def __str__(self):
+        match self:
+            case Role.SYSTEM:
+                return "<|system|>"
+            case Role.USER:
+                return "<|user|>"
+            case Role.ASSISTANT | Role.TOOL:
+                return "<|assistant|>"
+            case Role.OBSERVATION:
+                return "<|observation|>"
+
+    # Get the message block for the given role
+    def get_message(self):
+        # Compare by value here, because the enum object in the session state
+        # is not the same as the enum cases here, due to streamlit's rerunning
+        # behavior.
+        match self.value:
+            case Role.SYSTEM.value:
+                return
+            case Role.USER.value:
+                return st.chat_message(name="user", avatar="user")
+            case Role.ASSISTANT.value:
+                return st.chat_message(name="assistant", avatar="assistant")
+            case Role.TOOL.value:
+                return st.chat_message(name="tool", avatar="assistant")
+            case Role.OBSERVATION.value:
+                return st.chat_message(name="observation", avatar="assistant")
+            case _:
+                st.error(f"Unexpected role: {self}")
+
+
+@dataclass
+class Conversation:
+    role: Role
+    content: str | dict
+    # Processed content
+    saved_content: str | None = None
+    metadata: str | None = None
+    image: str | Image | None = None
+
+    def __str__(self) -> str:
+        metadata_str = self.metadata if self.metadata else ""
+        return f"{self.role}{metadata_str}\n{self.content}"
+
+    # Human readable format
+    def get_text(self) -> str:
+        text = self.saved_content or self.content
+        match self.role.value:
+            case Role.TOOL.value:
+                text = f"Calling tool `{self.metadata}`:\n\n```python\n{text}\n```"
+            case Role.OBSERVATION.value:
+                text = f"```python\n{text}\n```"
+        return text
+
+    # Display as a markdown block
+    def show(self, placeholder: DeltaGenerator | None = None) -> str:
+        if placeholder:
+            message = placeholder
+        else:
+            message = self.role.get_message()
+
+        if self.image:
+            message.image(self.image, width=512)
+
+        if self.role == Role.OBSERVATION:
+            metadata_str = f"from {self.metadata}" if self.metadata else ""
+            message = message.expander(f"Observation {metadata_str}")
+
+        text = self.get_text()
+        if self.role != Role.USER:
+            show_text = text
+        else:
+            splitted = text.split('files uploaded.\n')
+            if len(splitted) == 1:
+                show_text = text
+            else:
+                # Show expander for document content
+                doc = splitted[0]
+                show_text = splitted[-1]
+                expander = message.expander(f'File Content')
+                expander.markdown(doc)
+        message.markdown(show_text)
+
+
+def postprocess_text(text: str, replace_quote: bool) -> str:
+    text = text.replace("\(", "$")
+    text = text.replace("\)", "$")
+    text = text.replace("\[", "$$")
+    text = text.replace("\]", "$$")
+    text = text.replace("<|assistant|>", "")
+    text = text.replace("<|observation|>", "")
+    text = text.replace("<|system|>", "")
+    text = text.replace("<|user|>", "")
+    text = text.replace("<|endoftext|>", "")
+
+    return text.strip()
--- main.py
+++ main.py
@@ -1,22 +1,210 @@
-from agent import agent_execute_with_retry
-from llm import load_glm4
-import argparse
 import os
-import asyncio
+import traceback
+from enum import Enum
+from io import BytesIO
+from uuid import uuid4
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="qwen-turbo")
-    parser.add_argument("--CUDA_VISIBLE_DEVICES", type=str, default="0")
-    args = parser.parse_args()
+import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 
-    model = args.model
-    if model == 'glm4':
-        load_glm4()
-        os.environ["CUDA_VISIBLE_DEVICES"] = args.CUDA_VISIBLE_DEVICES
+from PIL import Image
 
-    my_history = []
-    while True:
-        query = input('query:')
-        success, result, my_history = asyncio.run(agent_execute_with_retry(query, chat_history=my_history, model=model))
-        my_history = my_history[-10:]
+from client import Client, ClientType, get_client
+from conversation import (
+    Conversation,
+    Role,
+    postprocess_text,
+    response_to_str,
+)
+from tools.tool_registry import dispatch_tool, get_tools
+
+CHAT_MODEL_PATH = "./models/glm-4-9b-chat"
+
+
+def append_conversation(
+        conversation: Conversation,
+        history: list[Conversation],
+        placeholder: DeltaGenerator | None = None,
+) -> None:
+    """
+    Append a conversation piece into history, meanwhile show it in a new markdown block
+    """
+    history.append(conversation)
+    conversation.show(placeholder)
+
+
+st.set_page_config(
+    page_title="GLM-4 Demo",
+    page_icon=":robot:",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+st.title("GLM-4 Agent")
+
+with st.sidebar:
+    top_p = st.slider("top_p", 0.0, 1.0, 0.8, step=0.01)
+    top_k = st.slider("top_k", 1, 20, 10, step=1, key="top_k")
+    temperature = st.slider("temperature", 0.0, 1.5, 0.95, step=0.01)
+    repetition_penalty = st.slider("repetition_penalty", 0.0, 2.0, 1.0, step=0.01)
+    max_new_tokens = st.slider("max_new_tokens", 1, 4096, 2048, step=1)
+    cols = st.columns(2)
+    export_btn = cols[0]
+    clear_history = cols[1].button("Clear", use_container_width=True)
+    retry = export_btn.button("Retry", use_container_width=True)
+
+if clear_history:
+    client = st.session_state.client
+    st.session_state.clear()
+    st.session_state.client = client
+    st.session_state.history = []
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+st.session_state.client = get_client(model_path=CHAT_MODEL_PATH, typ=ClientType.VLLM)
+
+prompt_text = st.chat_input("Chat with GLM-4!", key="chat_input")
+
+if prompt_text == "" and retry == False:
+    print("\n== Clean ==\n")
+    st.session_state.history = []
+    exit()
+
+history: list[Conversation] = st.session_state.history
+
+if retry:
+    print("\n== Retry ==\n")
+    last_user_conversation_idx = None
+    for idx, conversation in enumerate(history):
+        if conversation.role.value == Role.USER.value:
+            last_user_conversation_idx = idx
+    if last_user_conversation_idx is not None:
+        prompt_text = history[last_user_conversation_idx].content
+        print(f"New prompt: {prompt_text}, idx = {last_user_conversation_idx}")
+        del history[last_user_conversation_idx:]
+
+for conversation in history:
+    conversation.show()
+
+tools = get_tools()
+
+client: Client = st.session_state.client
+
+
+def main(prompt_text: str):
+    global client
+    assert client is not None
+
+    if prompt_text:
+        prompt_text = prompt_text.strip()
+
+        role = Role.USER
+        append_conversation(Conversation(role, prompt_text), history)
+
+        placeholder = st.container()
+        message_placeholder = placeholder.chat_message(
+            name="assistant", avatar="assistant"
+        )
+        markdown_placeholder = message_placeholder.empty()
+
+        def add_new_block():
+            nonlocal message_placeholder, markdown_placeholder
+            message_placeholder = placeholder.chat_message(
+                name="assistant", avatar="assistant"
+            )
+            markdown_placeholder = message_placeholder.empty()
+
+        def commit_conversation(
+                role: Role,
+                text: str,
+                metadata: str | None = None,
+                image: str | None = None,
+                new: bool = False,
+        ):
+            processed_text = postprocess_text(text, role.value == Role.ASSISTANT.value)
+            conversation = Conversation(role, text, processed_text, metadata, image)
+
+            # Use different placeholder for new block
+            placeholder = message_placeholder if new else markdown_placeholder
+
+            append_conversation(
+                conversation,
+                history,
+                placeholder,
+            )
+
+        response = ""
+        for _ in range(10):  # 最多调用10次tool
+            last_response = None
+            history_len = None
+
+            try:
+                for response, chat_history in client.generate_stream(
+                        tools=tools,
+                        history=history,
+                        temperature=temperature,
+                        top_p=top_p,
+                        top_k=top_k,
+                        repetition_penalty=repetition_penalty,
+                        max_new_tokens=max_new_tokens,
+                ):
+                    if history_len is None:
+                        history_len = len(chat_history)
+                    elif history_len != len(chat_history):
+                        commit_conversation(Role.ASSISTANT, last_response)
+                        add_new_block()
+                        history_len = len(chat_history)
+                    last_response = response
+                    replace_quote = chat_history[-1]["role"] == "assistant"
+                    markdown_placeholder.markdown(
+                        postprocess_text(
+                            str(response) + "●", replace_quote=replace_quote
+                        )
+                    )
+                else:
+                    metadata = (
+                            isinstance(response, dict)
+                            and response.get("name")
+                            or None
+                    )
+                    role = Role.TOOL if metadata else Role.ASSISTANT
+                    text = (
+                        response.get("content")
+                        if metadata
+                        else response_to_str(response)
+                    )
+                    commit_conversation(role, text, metadata)
+                    if metadata:
+                        add_new_block()
+                        try:
+                            with markdown_placeholder:
+                                with st.spinner(f"Calling tool {metadata}..."):
+                                    observations = dispatch_tool(
+                                        metadata, text
+                                    )
+                        except Exception as e:
+                            traceback.print_exc()
+                            st.error(f'Uncaught exception in `"{metadata}"`: {e}')
+                            break
+
+                        for observation in observations:
+                            observation.text = observation.text
+                            commit_conversation(
+                                Role.OBSERVATION,
+                                observation.text,
+                                observation.role_metadata,
+                                new=True,
+                            )
+                            add_new_block()
+                        continue
+                    else:
+                        break
+            except Exception as e:
+                traceback.print_exc()
+                st.error(f"Uncaught exception: {traceback.format_exc()}")
+        else:
+            st.error("Too many chaining function calls!")
+
+
+main(prompt_text)
